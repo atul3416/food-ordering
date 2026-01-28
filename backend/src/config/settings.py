08c084791d4376dcd25242aa7e7da80a2dec7e99
atul3416 <rajsvm02@gmail.com>
2026-01-28 23:38:24 +0530

    update setting.py to add cors header and django-rest-framework

--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -37,9 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'foodordering',
+    'corsheaders', 
+    'rest_framework',
 ]
 
+CORS_ALLOWED_ORIGINS = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
