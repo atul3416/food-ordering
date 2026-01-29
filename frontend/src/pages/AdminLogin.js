69b6cf53dc69740dec45c593398add6648390812
atul3416 <rajsvm02@gmail.com>
2026-01-30 02:26:06 +0530

    update login page

--- frontend/src/pages/AdminLogin.js
+++ frontend/src/pages/AdminLogin.js
@@ -1,30 +1,55 @@
-import React from 'react'
-import { FaUser, FaLock , FaSignIn, FaSignInAlt } from 'react-icons/fa'
+import React, { useState } from 'react'
+import { FaUser, FaLock, FaSignIn, FaSignInAlt } from 'react-icons/fa'
 import '../styles/admin.css'
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 const AdminLogin = () => {
-  return(
-    <div className='d-flex justify-content-center vh-100 align-items-center' style={{backgroundImage:"url('/images/foodbg.jpg')",backgroundSize:'cover'}}>
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const response = await fetch('http://127.0.0.1:8000/api/admin-login/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username, password })
+    });
 
-    <div className='card p-4 shadow-lg ' style={{maxWidth:"400px", width:"100%"}}>
-      <h4 className='text-center mb-4'> <FaUser className='me-2 icon-fix'/> AdminLogin</h4>
+    const data = await response.json();
+    if (response.status === 200) {
+      setTimeout(() => {
+        toast.success(data.message)
+        window.location.href = '/admin-dashboard';
+      }, 2000)
+    }
+    else {
+      toast.error(data.message);
+    }
+  }
+  return (
 
-      <form>
-        <div className='mb-3'>
-          <label className='form-label'><FaUser className='me-1 icon-fix'/> UserName</label>
-          <input type='text' className='form-control' placeholder='Enter Admin username' required/>  
-        </div>
+    <div className='d-flex justify-content-center vh-100 align-items-center' style={{ backgroundImage: "url('/images/foodbg.jpg')", backgroundSize: 'cover' }}>
 
-        <div className='mb-3'>
-          <label className='form-label'><FaLock className='me-1 icon-fix'/> Password</label>
-          <input type='password' className='form-control' placeholder='Enter password' required/>  
-        </div>
+      <div className='card p-4 shadow-lg ' style={{ maxWidth: "400px", width: "100%" }}>
+        <h4 className='text-center mb-4'> <FaUser className='me-2 icon-fix' /> AdminLogin</h4>
 
-        <button type='submit' className='btn btn-primary w-100 mt-3'>
-          <FaSignInAlt className='m3-1 icon-fix'/> Login
-        </button>
+        <form onSubmit={handleLogin}>
+          <div className='mb-3'>
+            <label className='form-label'><FaUser className='me-1 icon-fix' /> UserName</label>
+            <input type='text' className='form-control' value={username} onChange={(e) => setUsername(e.target.value)} placeholder='Enter Admin username' required />
+          </div>
 
-      </form>
-    </div>
+          <div className='mb-3'>
+            <label className='form-label'><FaLock className='me-1 icon-fix' /> Password</label>
+            <input type='password' className='form-control' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Enter password' required />
+          </div>
+
+          <button type='submit' className='btn btn-primary w-100 mt-3'>
+            <FaSignInAlt className='m3-1 icon-fix' /> Login
+          </button>
+
+        </form>
+      </div>
+      <ToastContainer autoClose={2000}  />
     </div>
   )
 }
